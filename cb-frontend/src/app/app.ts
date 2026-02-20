69ddd6a8970c511f11cf545873276d7874bbce18
MaxTrenhaile <max.trenhaile@virtualincision.com>
2026-02-20 11:17:07 -0800

    Swap angular project style to use scss, disable server side rendering for now (we can easily add it later)

--- cb-frontend/src/app/app.ts
+++ cb-frontend/src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('CB-frontend');
+  protected readonly title = signal('cb-frontend');
 }
